6537aa2d1303f60dd9dd0e37a933ef37a73ddb0c
PC-20140906HEPR\Administrator <814805645@qq.com>
2016-09-20 22:33:22 +0800

    Finish Homework 2

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 var i = 0;
 var j = 0;
-var n = 5;
+var n = 6;
 var line = 0;
-var a = "";
-var b = "";
-var c = " ";
-var d = "*";
+var x = "";
+var y = "";
+var w = " ";
+var z = "*";
 line = getLine(n);
 hua(line);
 function hua(n) {
     for (i = 0; i < n; i++) {
         for (j = 0; j < n - i; j++) {
-            a = a + c;
+            x = x + w;
         }
         for (j = 0; j < 2 * i + 1; j++) {
-            b = b + d;
+            y = y + z;
         }
-        console.log(a + b);
-        a = "";
-        b = "";
+        console.log(x + y);
+        x = "";
+        y = "";
     }
 }
 function getLine(line) {
--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
  var i : number = 0;
  var j : number = 0;
- var n : number = 5;
+ var n : number = 6;
  var line : number = 0;
  
- var a = "";
- var b = "";
- var c = " ";
- var d = "*";
+ var x = "";
+ var y = "";
+ var w = " ";
+ var z = "*";
 
  line = getLine(n);
  hua(line);
@@ -14,14 +14,14 @@
  function hua(n : number) : void{
      for(i = 0 ; i < n; i++){
          for(j = 0; j < n - i; j++){
-             a = a + c;
+             x = x + w;
          }
          for(j = 0; j < 2 * i + 1; j++){
-             b = b + d;
+             y = y + z;
          }
-         console.log(a + b);
-         a = ""
-         b = ""
+         console.log(x + y);
+         x = ""
+         y = ""
      }
  }
 
